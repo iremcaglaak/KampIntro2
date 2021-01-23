d9388639c906f49d4bfe8102b0d5d61e4589658e
iremcaglaak <77752265+iremcaglaak@users.noreply.github.com>
2021-01-23 19:24:58 +0300

    merhaba

--- Metotlar/Program.cs
+++ Metotlar/Program.cs
@@ -44,7 +44,10 @@ namespace Metotlar
             sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
             sepetManager.Ekle2("Elma", "Yeşil elma", 12, 9);
             sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 8);
+
+            Console.WriteLine("merhaba");
         }
+          
     }
 }
 
